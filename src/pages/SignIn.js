c6c5ae723705376c2e68a73e835aacbfa0e72ca0
Utku İpek <utkuipek07@hotmail.com>
2022-03-13 16:56:47 +0300

    Work on SignIn and SignUp pages

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,61 @@
-import React from "react";
+import {
+  TextField,
+  CssBaseline,
+  Button,
+  Box,
+  Container,
+  Link,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import { LockOutlined } from "@mui/icons-material";
 
 function SignIn() {
-  return <div>Hi, SignIn!</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="xs">
+        <Box component="form" sx={{ mt: 4 }}>
+          <Avatar sx={{ mx: "auto", bgcolor: "secondary.main" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography variant="h5" sx={{ textAlign: "center" }}>
+            Sign in
+          </Typography>
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Email Address"
+            required
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Password"
+            required
+            type="password"
+          />
+          <Button type="submit" variant="contained" fullWidth sx={{ mt: 2 }}>
+            Sign in
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              mt: 2,
+              cursor: "pointer",
+            }}
+          >
+            <Link>Forgot Password?</Link>
+            <Link>Don't have an account? Sign up</Link>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,67 @@
-import React from "react";
+import {
+  TextField,
+  CssBaseline,
+  Button,
+  Box,
+  Container,
+  Link,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import { LockOutlined } from "@mui/icons-material";
 
 function SignUp() {
-  return <div>Hi, SignUp!</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="xs">
+        <Box component="form" sx={{ mt: 4 }}>
+          <Avatar sx={{ mx: "auto", bgcolor: "secondary.main" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography variant="h5" sx={{ textAlign: "center" }}>
+            Sign up
+          </Typography>
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Full Name"
+            required
+            autoComplete="name"
+            autoFocus
+          />
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Email Address"
+            required
+            autoComplete="email"
+          />
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Password"
+            required
+            type="password"
+          />
+          <Button type="submit" variant="contained" fullWidth sx={{ mt: 2 }}>
+            Sign up
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              mt: 2,
+              cursor: "pointer",
+            }}
+          >
+            <Link>Already have an account? Sign in</Link>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default SignUp;
